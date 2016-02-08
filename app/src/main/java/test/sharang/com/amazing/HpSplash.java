d1d8bac385344535407fa6da0102396be805844b
Sharang Gupta <sharang.gupta@sitpune.edu.in>
2016-02-09 00:47:19 +0530

    7th commit,only ui and testing left!!!layout of app ready!!! more ui.......never seems to end!!! //just change intent links though....

--- app/src/main/java/test/sharang/com/amazing/HpSplash.java
+++ app/src/main/java/test/sharang/com/amazing/HpSplash.java
@@ -2,18 +2,29 @@ package test.sharang.com.amazing;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.VideoView;
 
 /**
  * Created by sharang on 2/8/16.
  */
 public class HpSplash extends Activity {
 
+    VideoView v;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hpsplash);
+
+        v=(VideoView) findViewById(R.id.vv);
+        String s= "android.resource://test.sharang.com.amazing/"+R.raw.hpsplashvv;
+        Uri u= Uri.parse(s);
+        v.setVideoURI(u);
+        v.requestFocus();
+        v.start();
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
--- app/src/main/java/test/sharang/com/amazing/PocSplash.java
+++ app/src/main/java/test/sharang/com/amazing/PocSplash.java
@@ -2,17 +2,28 @@ package test.sharang.com.amazing;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.VideoView;
 
 /**
  * Created by sharang on 2/8/16.
  */
 public class PocSplash extends Activity {
+    VideoView v;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pocsplash);
+
+        v=(VideoView) findViewById(R.id.vv);
+        String s= "android.resource://test.sharang.com.amazing/"+R.raw.pocsplashvv;
+        Uri u= Uri.parse(s);
+        v.setVideoURI(u);
+        v.requestFocus();
+        v.start();
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
